48eb56ff0181b0fa9d689112435e403bfaa0fe6d
hyoseon <hyoseon.jegal@frientrip.com>
2025-01-22 00:11:55 +0900

    find-the-prefix-common-array-of-two-arrays

---
+++ src/problems/leetcode/find-the-prefix-common-array-of-two-arrays.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun findThePrefixCommonArray(A: IntArray, B: IntArray): IntArray {
+        val resultArray = IntArray(A.size)
+        for( i in 0 until A.size){
+            // println("i: ${A[i]}")
+            for( j in 0 .. i){
+                for(x in  0..i){
+                    // println("j: ${B[j]}")
+                    if(A[j] == B[x]){
+                        resultArray[i]+=1
+                    }
+
+                }
+            }
+        }
+        return resultArray
+    }
+}
