5d37bd15b8c2d86f734975ab2693077585b6611e
hyoseon <hyoseon.dev@google.com>
2025-01-24 10:02:31 +0900

    [24]swap-nodes-in-pairs

---
+++ src/problems/leetcode/[24] swap-nodes-in-pairs.kt
@@ -0,0 +1,35 @@
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+
+    fun recur(parent: ListNode){
+        val first = parent.next
+        val second = parent.next.next
+        val third = parent.next.next.next
+
+        first.next = third
+        second.next = first
+        parent.next = second
+        if(first.next?.next != null){
+            recur(first)
+        }
+
+    }
+    fun swapPairs(head: ListNode?): ListNode? {
+        val dummy = ListNode(0)
+        dummy.next = head
+        if(dummy.next?.next != null){
+            recur(dummy)
+        }
+
+        return dummy.next
+    }
+}
